00a133a55d676add96126bfb12f72782777606d2
mastinux <andrea.pantaleo.93@gmail.com>
2015-07-04 09:27:47 +0200

    defined admin class

--- bidplacing/admin.py
+++ bidplacing/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class AuctionsUserAdmin(admin.ModelAdmin):
+    fields = ['user']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['category_name', 'parent', 'level']
+    list_display = ['category_name', 'parent',
+                    'is_top_category']
+    search_fields = ['category_name']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['product_name', 'description', 'start_price',
+              'deadline_time', 'seller', 'category']
+    list_display = ['product_name', 'description',
+                    'start_price', 'deadline_time', 'seller',
+                    'category']
+    list_filter = ('deadline_time',)
+    date_hierarchy = 'deadline_time'
+
+
+class BidAdmin(admin.ModelAdmin):
+    fields = ['product_name', 'bidder', 'amount',
+              'bidding_time']
+    list_display = ['product_name', 'bidder', 'amount',
+                    'bidding_time']
+
+admin.site.register(AuctionsUser, AuctionsUserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Bid, BidAdmin)
--- bidplacing/models.py
+++ bidplacing/models.py
@@ -8,27 +8,40 @@ class AuctionsUser(models.Model):
     # TODO integrare direttamente gli attributi di User
     # per evitare creazione della tabella auth_user
 
+    def __unicode__(self):
+        return self.user.username
+
+    def all(self):
+        AuctionsUser.objects.all()
+'''
     def __init__(self, user):
         self.user = user
     # TODO problemi nel salvare da django shell:
     # object has no attribute '_state'
-
-    def __unicode__(self):
-        return self.user.username
-
+'''
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
     parent = models.ForeignKey('self', null=True)
     level = models.IntegerField(default=0)
 
-    def __init__(self, category_name, parent=None, level=0):
+    def __unicode__(self):
+        return self.category_name
+
+    def all(self):
+        Category.objects.all()
+
+    def is_top_category(self):
+        if self.level == 0:
+            return True
+        else:
+            return False
+
+'''    def __init__(self, category_name, parent=None, level=0):
         self.category_name = category_name
         self.parent = parent
         self.level = level
-
-    def __unicode__(self):
-        return self.category_name
+'''
 
 
 class Product(models.Model):
@@ -39,6 +52,13 @@ class Product(models.Model):
     seller = models.ForeignKey(AuctionsUser)
     category = models.ForeignKey(Category)
 
+    def __unicode__(self):
+        return self.product_name
+
+    def all(self):
+        Product.objects.all()
+
+'''
     def __init__(self, product_name, description, start_price,
                  deadline_time, seller, category):
         self.product_name = product_name
@@ -47,10 +67,7 @@ class Product(models.Model):
         self.deadline_time = deadline_time
         self.seller = seller
         self.category = category
-
-    def __unicode__(self):
-        return self.product_name
-
+'''
 
 class Bid(models.Model):
     product_name = models.OneToOneField(Product)
@@ -58,12 +75,16 @@ class Bid(models.Model):
     amount = models.FloatField()
     bidding_time = models.DateTimeField('bidding time')
 
-    def __init__(self, product_name, bidder, amount,
+    def __unicode__(self):
+        return self.product_name, self.bidder, self.amount
+
+    def all(self):
+        Bid.objects.all()
+
+'''    def __init__(self, product_name, bidder, amount,
                  bidding_time):
         self.product_name = product_name
         self.bidder = bidder
         self.amount = amount
         self.bidding_time = bidding_time
-
-    def __unicode__(self):
-        return self.product_name, self.bidder, self.amount
+'''
